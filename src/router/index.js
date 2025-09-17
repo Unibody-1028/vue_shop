5ce48b7f9dfead830ccd6810c6ff3d9a5cab8451
unibody <guopp1028@gmail.com>
2025-09-18 02:24:20 +0800

    增加通过token判断用户是否登录的功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 
 ]
 
@@ -14,3 +16,11 @@ const router = new VueRouter({
 })
 
 export default router
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 无token情况下不允许访问login以外的地址
+  if (!tokenStr) return next('/login')
+  next()
+})
