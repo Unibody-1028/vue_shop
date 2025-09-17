fbf49068a1523da1ea7585cd340307ff22f08245
unibody <guopp1028@gmail.com>
2025-09-17 22:04:01 +0800

    登录组件布局设置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>登录组件</div>
+    <div class="login_container">
+      <div class="login_box"></div>
+    </div>
 </template>
 
 <script >
@@ -8,5 +10,18 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+  .login_container{
+    background-color: #b9e7e3;
+    height: 100%;
+  }
+  .login_box{
+    width: 450px;
+    height: 300px;
+    border-radius: 30px;
+    background-color:#fff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
 </style>
