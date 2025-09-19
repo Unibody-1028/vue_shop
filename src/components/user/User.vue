88b9690e3a4b190a8811b81ef8fca1ef49e0d471
unibody <guopp1028@gmail.com>
2025-09-19 17:48:17 +0800

    添加用户管理页面并优化全局样式

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,9 +3,51 @@
 </script>
 
 <template>
-    <div> 用户组件</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card >
+      <div style="margin-top: 5px;">
+        <el-row>
+          <el-col :span="8">
+            <el-input placeholder="请输入内容"  class="input-with-select">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </el-col>
+          <el-col :span="2">
+            <el-button type="primary" icon="el-icon-circle-plus-outline">新增用户</el-button>
+          </el-col>
+        </el-row>
+        <el-row >
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column prop="id" label="ID"></el-table-column>
+                <el-table-column prop="name" label="用户名"></el-table-column>
+                <el-table-column prop="nick_name" label="昵称"></el-table-column>
+                <el-table-column prop="phone" label="电话"></el-table-column>
+                <el-table-column prop="email" label="邮箱"></el-table-column>
+            </el-table>
+        </el-row>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+            style="display: flex; justify-content: center;" > //居中样式
+        </el-pagination>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <style scoped lang="less">
-
+.el-table{
+  margin-top: 10px
+}
 </style>
