753616ec173c5be62fc70d69dd350833e75d696d
unibody <guopp1028@gmail.com>
2025-09-22 10:27:37 +0800

    更新面包屑导航

--- src/components/power/Role.vue
+++ src/components/power/Role.vue
@@ -3,7 +3,7 @@
     <!--  面包屑  -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>角色管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!--    内容显示-->
