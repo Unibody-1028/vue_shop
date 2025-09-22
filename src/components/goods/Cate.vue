a44c61a2f9e67836e07e8b021e7c163a8709f1c5
unibody <guopp1028@gmail.com>
2025-09-23 01:42:20 +0800

    商品分类界面显示优化

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -16,8 +16,18 @@
         </el-col>
       </el-row>
       <el-row>
-        <tree-table :data="cateList" :columns="columns">
-
+        <tree-table :data="cateList" :columns="columns" :selection-type="false"
+                    :expand-type="false" class="tree-table" border="true"
+                    show-index="true">
+          <template slot="level" slot-scope="scope">
+            <el-tag v-if="scope.row.level===1">一级分类</el-tag>
+            <el-tag v-else-if="scope.row.level===2" type="success">二级分类</el-tag>
+            <el-tag v-else type="warning">三级分类</el-tag>
+          </template>
+          <template slot="opt" slot-scope="scope">
+            <el-button size="mini" icon="el-icon-edit" type="primary">编辑</el-button>
+            <el-button size="mini" icon="el-icon-delete" type="danger">删除</el-button>
+          </template>
         </tree-table>
       </el-row>
 
@@ -26,15 +36,21 @@
 </template>
 
 <style>
-
+.tree-table {
+  margin-top: 20px;
+}
 </style>
 
-<script >
+<script>
 export default {
   data() {
     return {
       cateList: [],
-      columns: [{label: '分类名称', prop: 'name'}]
+      columns: [
+        {label: '分类名称', prop: 'name'},
+        {label: '分类等级', type: 'template', template: 'level'},
+        {label: '操作', type: 'template', template: 'opt'}
+      ]
     }
   },
   created() {
