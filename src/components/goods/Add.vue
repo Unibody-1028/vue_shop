206abecce16abbfbde101831d72638df62fb1760
unibody <guopp1028@gmail.com>
2025-09-24 01:21:48 +0800

    添加增加商品页面的提示信息及优化标签页配置

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -7,8 +7,16 @@
       <el-breadcrumb-item>增加商品</el-breadcrumb-item>
     </el-breadcrumb>
 
-    <el-card>
-      <el-steps :active="active" finish-status="success">
+    <el-card >
+
+      <el-alert
+        title="增加商品信息"
+        type="success"
+        center
+        show-icon style="margin: 0 auto 10px;width: 600px;">
+      </el-alert>
+
+      <el-steps :active="active" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品静态参数"></el-step>
         <el-step title="商品动态参数"></el-step>
@@ -17,12 +25,12 @@
         <el-step title="完成"></el-step>
       </el-steps>
 
-      <el-tabs :tab-position="'left'" style="height: 200px;margin-top: 20px;margin-left: -10px">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品静态参数">商品静态参数</el-tab-pane>
-        <el-tab-pane label="商品动态参数">商品动态参数</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tabs v-model="active" :tab-position="'left'" style="height: 200px;margin-top: 20px;margin-left:-10px">
+        <el-tab-pane label="基本信息" name="1">基本信息</el-tab-pane>
+        <el-tab-pane label="商品静态参数" name="2">商品静态参数</el-tab-pane>
+        <el-tab-pane label="商品动态参数" name="3">商品动态参数</el-tab-pane>
+        <el-tab-pane label="商品图片" name="4">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="5">商品内容</el-tab-pane>
       </el-tabs>
 
     </el-card>
@@ -33,7 +41,7 @@
 export default {
   data() {
     return {
-      active: 0
+      active: '1'
     }
   }
 }
