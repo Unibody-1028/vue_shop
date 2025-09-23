743b369d08cd9a17f1154e0b10cdeca6f797d643
unibody <guopp1028@gmail.com>
2025-09-23 15:35:52 +0800

    更新 Attribute.vue 组件，添加动态和静态参数标签页及数据加载功能

--- src/components/goods/Attribute.vue
+++ src/components/goods/Attribute.vue
@@ -25,10 +25,10 @@
       </el-row>
       <el-row>
         <el-col>
-          <el-tabs>
-            <el-tab-pane label="静态参数" name="first">静态参数
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="静态参数" name="static">静态参数
               <el-button type="primary" size="mini">增加参数</el-button>
-              <el-table>
+              <el-table :data="staticAttr">
                 <el-table-column type="index"></el-table-column>
                 <el-table-column label="参数名称" prop="name"></el-table-column>
                 <el-table-column label="操作" prop="name">
@@ -39,9 +39,9 @@
                 </el-table-column>
               </el-table>
             </el-tab-pane>
-            <el-tab-pane label="动态参数" name="second">动态参数
+            <el-tab-pane label="动态参数" name="dynamic">动态参数
               <el-button type="primary" size="mini">增加参数</el-button>
-              <el-table>
+              <el-table :data="dynamicAttr">
                 <el-table-column type="index"></el-table-column>
                 <el-table-column label="参数名称" prop="name"></el-table-column>
                 <el-table-column label="操作" prop="name">
@@ -67,7 +67,10 @@ export default {
   data() {
     return {
       cateIdList: [],
-      selectKeys: []
+      selectKeys: [],
+      activeName: 'static',
+      staticAttr: [],
+      dynamicAttr: []
     }
   },
   created() {
@@ -81,13 +84,27 @@ export default {
     },
     changeSeletor() {
       console.log(this.selectKeys)
+    },
+    handleClick() {
+      // console.log(this.activeName)
+      if (this.selectKeys < 3) return
+      // console.log(this.selectKeys[2])
+      this.getAttribute()
+    },
+    async getAttribute() {
+      const {data: resp} = await this.$axios.get('/category/attr_list',
+        {params: {cid: this.selectKeys[2], type: this.activeName}})
+      if (resp.status !== 200) return this.$msg.error(resp.msg)
+      if (this.activeName === 'static') {
+        this.staticAttr = resp.data
+      } else {
+        this.dynamicAttr = resp.data
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-.el-tabs {
-  margin-top: 5px;
-}
+
 </style>
