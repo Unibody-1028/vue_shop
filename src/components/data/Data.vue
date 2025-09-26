6761b6d2c78607947b03845861d0a32d2ec5e9c6
unibody <guopp1028@gmail.com>
2025-09-27 01:36:52 +0800

    更新商品统计页面，调整图表尺寸并优化数据展示

--- src/components/data/Data.vue
+++ src/components/data/Data.vue
@@ -7,32 +7,42 @@
       <el-breadcrumb-item>商品统计</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <div id="main" style="width: 600px;height: 400px;"></div>
+      <div id="main" style="width: 1080px;height: 810px;margin:0 auto"></div>
     </el-card>
   </div>
 </template>
-<script >
+<script>
 import * as echarts from 'echarts'
+
 export default {
-  mounted() {
+  async mounted() {
+    const {data: resp} = await this.$axios.get('/cate_group_level')
+    if (resp.status !== 200) return this.$msg.error(resp.msg)
     var myChart = echarts.init(document.getElementById('main'))
     var option = {
       title: {
-        text: 'ECharts Getting Started Example'
+        text: '商品分类级别统计',
+        left: 'center'
       },
-      tooltip: {},
+      tooltip: {
+        trigger: 'axis', // 鼠标悬浮时显示提示框
+        axisPointer: {type: 'shadow'} // 柱状图提示线样式
+      },
       legend: {
-        data: ['sales']
+        data: [resp.data.name]
       },
       xAxis: {
-        data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+        name: '分类级别',
+        data: resp.data.xAxis
       },
-      yAxis: {},
+      yAxis: {
+        name: '数量(个)'
+      },
       series: [
         {
-          name: 'sales',
+          name: resp.data.name,
           type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
+          data: resp.data.series_data
         }
       ]
     }
